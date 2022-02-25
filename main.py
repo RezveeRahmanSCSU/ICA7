81e6a81bd37a66be4742526a103b3eab4e420347
Rezvee_Rahman_SCSU <guest5521@gmail.com>
2022-02-24 22:01:39 -0500

    added main

---
+++ main.py
@@ -0,0 +1,9 @@
+#Rezvee Rahman
+#CSC330-01
+#In class activity 7
+
+def main():
+    print("Hi Art, I have made a special git repo")
+   # print("add a print statement and let me know.")
+    #print here
+    return 0
